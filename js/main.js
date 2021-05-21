ada51aaa400aa30a7e88f955987a2bb40606d0e9
Victor Akpan <vicjust2011@gmail.com>
2021-05-21 22:36:09 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+ScrollReveal().reveal('.card_1', {
+  mobile:false,
+  direction:'bottom',
+  duration:1500,
+  distance:'95%'
+});
+ScrollReveal().reveal('.card_2', {
+  mobile:false,
+  direction:'bottom',
+  duration:1500,
+  distance:'100%',
+  delay:'500'
+});
+ScrollReveal().reveal('.card_3', {
+  mobile:false,
+  direction:'bottom',
+  duration:1500,
+  distance:'100%',
+  delay:750
+});
+
+
+
+
+
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'header': ['Source Sans Pro', 'sans-serif'],
+      'body': ['Nunito', 'sans-serif'],
+      'other': ['PT Sans', 'sans-serif']
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
